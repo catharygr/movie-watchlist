1e922a9008a793daf6709318413fb5564c68caf8
cathy <cathy@bubulazi.com>
2022-08-02 12:09:33 +0100

    JS de la busqueda inicial y de los detalles en el modal terminado

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const mainContainer =document.querySelector('#main-container')
 const searchBtn = document.querySelector('#search-btn')
 const cardRemoveBtn = document.querySelector('#card-remove-btn')
 const cardAddBtn = document.querySelector('#card-add-btn')
+const modal = document.querySelector('#modal')
 searchBtn.addEventListener('click', searchMovie)
 
 async function fetchData(searchInput, type) {
@@ -13,22 +14,52 @@ async function fetchData(searchInput, type) {
 
 function addEventBtnDetails() {
   const allDetailsBtn = document.querySelectorAll('#card-details-btn')
-   const btnArray = [...allDetailsBtn]
+  const btnArray = [...allDetailsBtn]
   btnArray.map(
     btn => btn.addEventListener('click', () => {
       fetchData(btn.dataset.title, 't')
-      .then(data => console.log(data))
+      .then(data => {
+        getDetailsHTML(data)
+        modal.showModal()
+        const closeModalBtn = document.querySelector('#close-modal-btn')
+        closeModalBtn.addEventListener('click', () => modal.close())
+      })
     })
   )
-  // console.log([btnArray])
+  
 }
+
+function getDetailsHTML(data) {
+  modal.innerHTML = `
+  <section class="card">
+    <div class="card-poster"><img src="${data.Poster}" alt="Movie Poster"></div>
+    <div class="card-meta">
+      <div class="card-title">
+        <h2>${data.Title}</h2>
+        <p>⭐️ <span>${data.imdbRating}</span></p>
+      </div>
+      <div class="card-details">
+        <p>${data.Runtime}</p>
+        <p>${data.Genre}</p>
+        <button id="card-add-btn"  class="card-btn"><span>+</span> Watchlist</button>
+      </div>
+      <div class="card-synopsis">
+        <p>${data.Plot}</p>
+      </div>
+    </div>
+    <button  id="close-modal-btn"  class="close-modal-btn">Close</button>
+  </section>  
+  `
+}
+
+
 
 function getSearchHTML(data) {
   const searcHTML = data.map(
     singleMovie =>  {
       return ` <section class="search-card">
       <div>
-        <div class="search-card-poster"><img src="${singleMovie.Poster}" alt="Algo asi"></div>
+        <div class="search-card-poster"><img src="${singleMovie.Poster}" alt="Movie Poster"></div>
       </div>
       <div class="search-card-meta">
         <button data-title="${singleMovie.Title}" class="search-card-btn" id="card-details-btn">Get details...</button>
